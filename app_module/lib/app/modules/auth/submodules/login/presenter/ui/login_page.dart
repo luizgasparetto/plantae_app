b03bf7610db498cd40ed052522637c5ff993b657
Luiz Gasparetto <luizegasp@gmail.com>
2022-10-06 01:04:29 -0300

    feat: add LoginPage and RegisterPage

--- app_module/lib/app/modules/auth/submodules/login/presenter/ui/login_page.dart
+++ app_module/lib/app/modules/auth/submodules/login/presenter/ui/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:core_module/core.dart';
+import 'package:dependency_module/dependency_module.dart';
 import 'package:flutter/material.dart';
 import 'package:plant_design_system/plant_design_system.dart';
 
@@ -15,38 +17,50 @@ class _LoginPageState extends State<LoginPage> {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            children: [
-              SizedBox(height: size.height * 0.1),
-              Text('Hello Again!', style: theme.textTheme.headline1),
-              Text("Welcome back you've been missed!", style: theme.textTheme.headline2),
-              SizedBox(height: size.height * 0.08),
-              Form(
-                child: Column(
-                  children: [
-                    const PlantTextFieldWidget(hintText: 'E-mail'),
-                    SizedBox(height: size.height * 0.02),
-                    const PlantTextFieldWidget(hintText: 'Password'),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: PlantTextButtonWidget(
-                        buttonLabel: 'Recovery Password',
-                        onPressed: () {},
-                      ),
-                    ),
-                    SizedBox(height: size.height * 0.06),
-                    PlantPrimaryButtonWidget(
-                      buttonLabel: 'Sign In',
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello Again!', style: theme.textTheme.headline1),
+            Text("Welcome back you've been missed!", style: theme.textTheme.headline2),
+            SizedBox(height: size.height * 0.08),
+            Form(
+              child: Column(
+                children: [
+                  const PlantTextFieldWidget(hintText: 'E-mail'),
+                  SizedBox(height: size.height * 0.02),
+                  const PlantTextFieldWidget(hintText: 'Password'),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: PlantTextButtonWidget(
+                      buttonLabel: 'Recovery Password',
                       onPressed: () {},
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.06),
+                  PlantPrimaryButtonWidget(
+                    buttonLabel: 'Sign In',
+                    onPressed: () => Modular.to.navigate(AppRoutes.dashboard),
+                  ),
+                  SizedBox(height: size.height * 0.03),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an account? ", style: theme.textTheme.headline5),
+                      InkWell(
+                        onTap: () => Modular.to.navigate(AppRoutes.registerPage),
+                        child: Text(
+                          'Sign Up',
+                          style: theme.textTheme.headline5!.copyWith(fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
--- core_module/lib/src/constants/routes/app_routes.dart
+++ core_module/lib/src/constants/routes/app_routes.dart
@@ -1,3 +1,9 @@
 class AppRoutes {
+  // AUTH
+  static const String loginPage = '/login';
+  static const String registerPage = '/register';
+
+  static const String dashboard = '/dashboard';
+
   static const String plantDetails = '/plant/details';
 }
